0b779777b7cc55ecde05265b442d1834d5cb4c13
Alex <mntokhirjonov@gmail.com>
2026-01-04 15:36:45 +0500

    create Components related business models

--- apps/medbook-api/src/components/components.module.ts
+++ apps/medbook-api/src/components/components.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { DoctorsModule } from './doctors/doctors.module';
+import { AppoinmentsModule } from './appoinments/appoinments.module';
+import { PrescriptionsModule } from './prescriptions/prescriptions.module';
+import { MedicalRecordsModule } from './medical-records/medical-records.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { ChatModule } from './chat/chat.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [MemberModule, DoctorsModule]
+  imports: [
+    MemberModule, 
+    DoctorsModule, 
+    AppoinmentsModule, 
+    PrescriptionsModule, 
+    MedicalRecordsModule, 
+    ReviewsModule, 
+    NotificationsModule, 
+    ChatModule, 
+    EmailModule,
+  ],
 })
 export class ComponentsModule {}
