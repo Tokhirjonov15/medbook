618aa1ddb16c7404fadf79dba2e574d77d4bff4d
Alex <mntokhirjonov@gmail.com>
2026-01-05 14:18:14 +0500

    feat: integration member Schema Model to membermodule component

--- apps/medbook-api/src/components/member/member.module.ts
+++ apps/medbook-api/src/components/member/member.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MemberService } from './member.service';
 import { MemberResolver } from './member.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import UserSchema from '../../schemas/Member.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{name: "Member", schema: UserSchema}])
+  ],
   providers: [MemberService, MemberResolver]
 })
 export class MemberModule {}
