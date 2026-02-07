b8c2179f8ed8dcf0566dca4ba5f44eef7bdc7d33
Alex <mntokhirjonov@gmail.com>
2026-02-07 20:09:01 +0900

    feat: create batchRollback, batchDoctors job schedules

--- apps/medbook-batch/src/batch.service.ts
+++ apps/medbook-batch/src/batch.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BatchService {
+	public async batchRollback(): Promise<void> {
+		console.log('batchRollback');
+	}
+
+	public async batchDoctors(): Promise<void> {
+		console.log('batchDoctors');
+	}
+
 	getHello(): string {
-		return 'Welcome to Medbook batch API Server!';
+		return 'Welcome to Nestar BATCH Server!';
 	}
 }
